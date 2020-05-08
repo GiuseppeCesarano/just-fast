f99d6ca8e0af160c56893b9d4d61ea3dc9d6d778
Giuseppe Cesarano <pecorainpannacotta@gmail.com>
2020-05-08 12:30:06 +0200

    Implemented general argument parser and help messange

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,48 @@
 #include "JustFastUi/JustFastUi.h"
+#include <cxxopts.hpp>
 #include <ftxui/component/screen_interactive.hpp>
+#include <iostream>
 
-int main()
+cxxopts::ParseResult parseArgs(cxxopts::Options opts, int* argc, char*** argv)
+{
+    try {
+        cxxopts::ParseResult res = opts.parse(*argc, *argv);
+        return res;
+    } catch (const cxxopts::OptionParseException& e) {
+        std::cerr << e.what() << std::endl;
+        exit(1);
+    }
+}
+
+void startJustFast()
 {
     auto screen = ftxui::ScreenInteractive::Fullscreen();
 
     JustFastUi ui;
     ui.setQuitFunction(screen.ExitLoopClosure());
     screen.Loop(&ui);
+}
+
+int main(int argc, char* argv[])
+{
+
+    cxxopts::Options cliOptions(PROJECT_NAME, PROJECT_DESCRIPTION);
+
+    // clang-format off
+
+    cliOptions.add_options()
+	("h,help", "Prints this help messange");
+
+    // clang-format on
+
+    auto cliResult = parseArgs(cliOptions, &argc, &argv);
+
+    if (cliResult.count("help")) {
+        std::cout << cliOptions.help() << std::endl;
+        exit(0);
+    }
+
+    startJustFast();
+
     return 0;
 }
